759f34c8c9af3e6b90af997ac60ac495af679960
MustapheMS <moztaphem9@gmail.com>
2023-06-15 22:19:37 +0300

    capitalie Done 100%

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Good Luck 💪🏾
+
+//Done 💯💯💯
+
+function makeCapital(str) {
+    let words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+  }
+  console.log(makeCapital("my name is ali"));
+  console.log(makeCapital("fikrcamp bootcamp"));
+  console.log(makeCapital("i live In hargeisa"));
